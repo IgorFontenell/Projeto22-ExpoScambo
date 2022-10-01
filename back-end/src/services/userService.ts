f08ae6089e08bac914b3c713dfc297f6eab3039b
Igor <igorfontenelletexeira@gmail.com>
2022-10-01 01:35:18 -0300

    Create User service done!

--- back-end/src/services/userService.ts
+++ back-end/src/services/userService.ts
@@ -1,5 +1,5 @@
 import { validateSchemas } from "../middlewares/schemaValidator";
-//import { userRepository }  from "../repositories/userRepository";
+import { userRepository }  from "../repositories/userRepository";
 import { userSchema } from "../schemas/userSchemas";
 import { IUserRegister, IUserLogin} from '../types/userTypes';
 import { decrypt, encrypt } from "../utils/criptUtils";
@@ -10,12 +10,18 @@ import jwt from 'jsonwebtoken';
 async function createUser (userInfo: IUserRegister) {
 
     await validateSchemas(userSchema.userRegisterSchema, userInfo); // Validating the stucture of the information send by the front.
-    const passwordEncrypted: string = encrypt(userInfo.password); // Encrypting the password so it can be storage in the DB safely.
 
-    //const user: any = await userRepository.getUserByEmail(userInfo.email); // Looking for the user by his email.
+    let user: any = await userRepository.getUserByEmail(userInfo.email); // Looking for the user by his email.
     
-    //if(user) {
-    //    throw {type: "not_acceptable", message: "User already exist!"}
-   // }
+    if(user) {
+        throw {type: "not_acceptable", message: "Email already registered!"}
+    }
+
+    user = await userRepository.getUserByCpf(userInfo.cpf); // Looking for the user by his  cpf.
+    if(user) {
+        throw {type: "not_acceptable", message: "Cpf already registered!"}
+    }
+    
+    const passwordEncrypted: string = encrypt(userInfo.password); // Encrypting the password so it can be storage in the DB safely.
     await userRepository.createUser(userInfo, passwordEncrypted); // Creating the user in the DB.
 }
