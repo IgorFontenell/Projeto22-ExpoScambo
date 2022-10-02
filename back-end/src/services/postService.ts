c02f47ebaf6cda05a2e6d7b4e66a8cd701176ad2
Igor <igorfontenelletexeira@gmail.com>
2022-10-02 20:24:53 -0300

    Create post service done

--- back-end/src/services/postService.ts
+++ back-end/src/services/postService.ts
@@ -4,17 +4,16 @@ import { postSchema } from '../schemas/postSchemas'
 import { TypeCreatePost } from '../types/postTypes'
 
 
-async function create (postInfo: TypeCreatePost) {
+async function create (postInfo: TypeCreatePost, userId: number) {
     await validateSchemas(postSchema.postCreateSchema, postInfo);
     const lookingForCategory = await postRepository.findCategoryByName(postInfo.categoryName);
 
     if(!lookingForCategory) {
         throw {type: "not_found", message: "Category dosen't exist"}
     }
+    await postRepository.createPost(postInfo, userId);
     
-    
-
-
+    return;
 }
 
 export const postService = {
