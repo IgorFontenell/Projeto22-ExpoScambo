ac594b34a0412c5c8516e6103ceb755c8175f53f
Igor <igorfontenelletexeira@gmail.com>
2022-10-02 12:35:51 -0300

    Validating the format of the information comming from the postController

--- back-end/src/services/postService.ts
+++ back-end/src/services/postService.ts
@@ -1,9 +1,10 @@
 import { validateSchemas } from '../middlewares/schemaValidator'
+import { postSchema } from '../schemas/postSchemas'
 import { TypeCreatePost } from '../types/postTypes'
 
 
 async function create (postInfo: TypeCreatePost) {
-    
+    await validateSchemas(postSchema.postCreateSchema, postInfo);
 }
 
 export const postService = {
