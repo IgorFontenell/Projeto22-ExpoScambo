aa52f6f75037ae91f81af6fbf7830c76360e97bd
Igor <igorfontenelletexeira@gmail.com>
2022-10-03 19:24:52 -0300

    Get all chats messages service created

--- back-end/src/services/chatService.ts
+++ back-end/src/services/chatService.ts
@@ -5,10 +5,14 @@ async function getAll (userId: number) {
     const allMessages = await chatRepository.getAllChats(userId);
     return allMessages;
 }
+async function getAllMessages (userId: number) {
+    const allMessages = await chatRepository.getAllChats(userId);
+    return allMessages;
+}
 
 
 export const chatService = {
     getAll,
-    
+    getAllMessages
     
 }
