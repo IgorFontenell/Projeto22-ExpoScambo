4b8423ba08455bdc0cba62b20a445b4380fc8618
Igor <igorfontenelletexeira@gmail.com>
2022-10-02 22:13:02 -0300

    Adding one more error to the error middleware

--- back-end/src/middlewares/errorHandler.ts
+++ back-end/src/middlewares/errorHandler.ts
@@ -7,7 +7,7 @@ export default function errorHandler (error: any, request: Request, response: Re
   if (error.type === "not_found") return response.status(404).send(error.message);
   if(error.type === "not_acceptable") return response.status(406).send(error.message);
   if (error.type === "unprocessable_entity") return response.status(422).send(error.message);
-
+  if (error.type === "server_error") return response.status(500).send(error.message);
  
   response.status(500).send("Server error");
    // internal server error
