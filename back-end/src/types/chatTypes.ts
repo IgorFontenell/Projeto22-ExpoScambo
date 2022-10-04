4ffb7e06ca8f4bf059bdb5c3a63802bf69990509
Igor <igorfontenelletexeira@gmail.com>
2022-10-03 22:12:31 -0300

    Created the chatDB interface

---
+++ back-end/src/types/chatTypes.ts
@@ -0,0 +1,15 @@
+
+export interface IChatDB {
+    id: number;
+    courierId: number
+    buyerId: number
+}
+
+// export interface ICategoryDB {
+//     id: number;
+//     name: string;
+// }
+
+
+
+//export type TypeCreatePost = Omit<IPostDB, "id" & "userId">
