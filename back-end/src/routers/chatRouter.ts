7c436352bbe8a25fef372e61f3fd82afd07e65d4
Igor <igorfontenelletexeira@gmail.com>
2022-10-03 21:40:10 -0300

    Send message route done

--- back-end/src/routers/chatRouter.ts
+++ back-end/src/routers/chatRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAllChatsController, getAllMessagesController } from "../controllers/chatController";
+import { getAllChatsController, getAllMessagesController, sendMessage } from "../controllers/chatController";
 import { autenticateMiddleware } from "../middlewares/authMiddleware";
 
 
@@ -7,7 +7,7 @@ const chat = Router();
 
 chat.get("/chat/all", autenticateMiddleware, getAllChatsController);
 chat.get("/chat/:destinyId", autenticateMiddleware, getAllMessagesController);
-chat.post("/chat/message/:destinyId", autenticateMiddleware, );
+chat.post("/chat/message/:destinyId", autenticateMiddleware, sendMessage);
 
 
 export default chat;
