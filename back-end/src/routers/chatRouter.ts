ef03d2694f500fabd9355a41c20c2f9356ca4ee5
Igor <igorfontenelletexeira@gmail.com>
2022-10-03 21:05:07 -0300

    Get all chats messages route and controller done

--- back-end/src/routers/chatRouter.ts
+++ back-end/src/routers/chatRouter.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { getAllChatsController } from "../controllers/chatController";
+import { getAllChatsController, getAllMessagesController } from "../controllers/chatController";
 import { autenticateMiddleware } from "../middlewares/authMiddleware";
 
 
 const chat = Router();
 
 chat.get("/chat/all", autenticateMiddleware, getAllChatsController);
-chat.get("/chat/messages/:destinyId", );
+chat.get("/chat/messages/:destinyId", autenticateMiddleware, getAllMessagesController);
 
 export default chat;
