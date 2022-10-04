72f71599182155511a4430c5c6c47def11b8d522
Igor <igorfontenelletexeira@gmail.com>
2022-10-03 21:13:23 -0300

    Get all messages between 2 users corrected with an error type

--- back-end/src/services/chatService.ts
+++ back-end/src/services/chatService.ts
@@ -1,11 +1,17 @@
 import { validateSchemas } from '../middlewares/schemaValidator';
 import { chatRepository } from '../repositories/chatRepository';
+import { userRepository } from '../repositories/userRepository';
 
 async function getAll (userId: number) {
     const allMessages = await chatRepository.getAllChats(userId);
     return allMessages;
 }
 async function getAllMessages (userId: number, destinyMessageId: number) {
+
+    const destinyUser = await userRepository.findById(destinyMessageId);
+    if(!destinyUser) {
+        throw {type: "not_found", message: "User dosen't exist"}
+    }
     const allMessages = await chatRepository.getAllMessagesBetween2User(userId, destinyMessageId);
     return allMessages;
 }
