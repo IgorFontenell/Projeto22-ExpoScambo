9bc28e115de45d5cd6473318e915df05509414bf
Igor <igorfontenelletexeira@gmail.com>
2022-10-10 02:20:01 -0300

    Changing some routes to do not need autentication

--- back-end/src/routers/postRouter.ts
+++ back-end/src/routers/postRouter.ts
@@ -6,8 +6,8 @@ import { autenticateMiddleware } from "../middlewares/authMiddleware";
 const post = Router();
 
 post.post("/posts/create", autenticateMiddleware, createPostController);
-post.get("/posts/:id", autenticateMiddleware, getPostById);
-post.get("/category/:category", autenticateMiddleware, getPostsByCategory);
+post.get("/posts/:id", getPostById);
+post.get("/category/:category", getPostsByCategory);
 
 
 export default post;
